9425f149a840881da26b713012d066eb8d4d2488
zahareus <zahareus@gmail.com>
2025-05-31 14:45:02 +0200

    Create airtable-config.js

---
+++ js/airtable-config.js
@@ -0,0 +1,6 @@
+// js/airtable-config.js
+
+const AIRTABLE_BASE_ID = 'appc8pqZEUQibc8ya'; // Наприклад: 'appXXXXXXXXXXXXXX'
+const AIRTABLE_PERSONAL_ACCESS_TOKEN = 'patp3znFXZRtQ3xoV.f8696ec94eb981429eac295402160434e24780c83be85fb7666a38cc31a2df0a'; // Наприклад: 'patYYYYYYYYYYYYYY.abcdefg...'
+
+// Переконайтеся, що значення взяті в одинарні або подвійні лапки.
